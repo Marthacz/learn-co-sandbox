d5917f8eee21d1f37f2d16d5ae25621674b09f52
Martha Czernuszenko <mvczer@gmail.com>
2018-08-07 16:24:35 +0000

    Done.

---
+++ ella.rb
@@ -0,0 +1,23 @@
+# puts "Who are you introducing to?"
+# $secondname = gets.chomp
+
+# def userinputmethod(name,nameagain = $secondname)
+#   puts "Hello my #{name}. Your name is #{nameagain}"
+# end 
+
+# userinputmethod("Martha")
+
+
+
+#Cannot name variables with a number
+
+
+
+#name = "Martha"
+
+def say_hello(name = "Ruby Programmer")
+  #name = "Lucy"
+  puts "Hello my name is #{name}"
+end
+say_hello("Alicia")
+  
